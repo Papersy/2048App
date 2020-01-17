afdc6ec59eaf0415744b11a8a1d7a927399334c5
Papersy <v.grishaenko@ukr.net>
2020-01-17 23:03:14 +0200

    1

---
+++ app/src/main/java/com/example/a2048app/mainGameWindow/gameWindow.java
@@ -0,0 +1,16 @@
+package com.example.a2048app.mainGameWindow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a2048app.R;
+
+public class gameWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_window);
+    }
+}
